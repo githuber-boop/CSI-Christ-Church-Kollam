b506f9bbd1e67246a806a2864ec4b4e2f9be898f
Ashish <ashishalex084@gmail.com>
2024-09-12 17:19:28 +0530

    mainproddd paste changes

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -28,7 +28,7 @@ const AdminDashboard = () => {
 
   const handlePaste = (e) => {
     e.preventDefault(); // Prevent the default paste behavior
-    const pastedText = e.clipboardData.getData('text');
+    const pastedText = e.clipboardData.getData("text");
 
     // Regular expression to match dd/mm/yyyy format
     const datePattern = /^\d{2}\/\d{2}\/\d{4}$/;
@@ -38,19 +38,18 @@ const AdminDashboard = () => {
       setFormData({ ...formData, [name]: pastedText });
       // Set the date if format is correct
     } else {
-      alert('Invalid date format! Please use dd-mm-yy');
+      alert("Invalid date format! Please use dd-mm-yy");
     }
   };
 
-
   const handleChange = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
   const handleFamilyMemberChange = (index, e) => {
-    e.preventDefault()
+    e.preventDefault();
     const { name, value } = e.target;
     const updatedFamilyMembers = formData.familyMembers.map((member, i) =>
       i === index ? { ...member, [name]: value } : member
@@ -58,18 +57,27 @@ const AdminDashboard = () => {
     setFormData({ ...formData, familyMembers: updatedFamilyMembers });
   };
 
-  const  addFamilyMember= () => {
+  const addFamilyMember = () => {
     setFormData({
       ...formData,
       familyMembers: [
         ...formData.familyMembers,
-        { name: "", dob: "", confirmation: "", baptism: "", confirmed: false, baptised: false },
+        {
+          name: "",
+          dob: "",
+          confirmation: "",
+          baptism: "",
+          confirmed: false,
+          baptised: false,
+        },
       ],
     });
   };
 
   const removeFamilyMember = (index) => {
-    const updatedFamilyMembers = formData.familyMembers.filter((_, i) => i !== index);
+    const updatedFamilyMembers = formData.familyMembers.filter(
+      (_, i) => i !== index
+    );
     setFormData({ ...formData, familyMembers: updatedFamilyMembers });
   };
 
@@ -124,7 +132,10 @@ const AdminDashboard = () => {
   const submitForm = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://church-kollam-backend.onrender.com/api/users/newUser", formData);
+      const response = await axios.post(
+        "https://church-kollam-backend.onrender.com/api/users/newUser",
+        formData
+      );
       console.log(response.data);
     } catch (error) {
       console.error(error);
@@ -140,68 +151,235 @@ const AdminDashboard = () => {
         <h1>CREATE A NEW USER</h1>
         <form onSubmit={submitForm} className="new_user-form">
           {/* Main User Fields */}
-          <label htmlFor="name">Name: <input type="text" name="name" value={formData.name} onChange={handleChange} /></label>
-          <label htmlFor="address">Address: <input type="text" name="address" value={formData.address} onChange={handleChange} /></label>
-          <label htmlFor="number">Phone Number: <input type="text" name="number" value={formData.number} onChange={handleChange} /></label>
-          <label htmlFor="dob">Date of Birth: <input readOnly onPaste={handlePaste} type="text" name="dob" value={formData.dob} onChange={handleChange} /></label>
-          <label htmlFor="weddingDte">Wedding Date: <input readOnly onPaste={handlePaste} type="text" name="weddingDte" value={formData.weddingDte} onChange={handleChange} /></label>
-          <label htmlFor="email">Email: <input type="text" name="email" value={formData.email} onChange={handleChange} /></label>
-          
+          <label htmlFor="name">
+            Name:{" "}
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="address">
+            Address:{" "}
+            <input
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="number">
+            Phone Number:{" "}
+            <input
+              type="text"
+              name="number"
+              value={formData.number}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="dob">
+            Date of Birth:{" "}
+            <input
+              readOnly
+              onPaste={handlePaste}
+              type="text"
+              name="dob"
+              value={formData.dob}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="weddingDte">
+            Wedding Date:{" "}
+            <input
+              readOnly
+              onPaste={handlePaste}
+              type="text"
+              name="weddingDte"
+              value={formData.weddingDte}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="email">
+            Email:{" "}
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+
           {/* Main User Baptism & Confirmation */}
           <label htmlFor="baptism">
             Baptism Date:
             {!baptised ? (
-              <input type="text" readOnly onPaste={handlePaste} name="baptism" value={formData.baptism} onChange={handleChange} />
+              <input
+                type="text"
+                readOnly
+                onPaste={handlePaste}
+                name="baptism"
+                value={formData.baptism}
+                onChange={handleChange}
+              />
             ) : (
-              <input type="text" name="baptism" value={formData.baptism} disabled />
+              <input
+                type="text"
+                name="baptism"
+                value={formData.baptism}
+                disabled
+              />
             )}
-            <div className="block-container"><label><input type="checkbox" className="checkbox" checked={baptised} onChange={handleMainBaptisedToggle} /> Mark as "Baptised"</label></div>
+            <div className="block-container">
+              <label>
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  checked={baptised}
+                  onChange={handleMainBaptisedToggle}
+                />{" "}
+                Mark as "Baptised"
+              </label>
+            </div>
           </label>
           <label htmlFor="confirmation">
             Confirmation Date:
             {!confirmed ? (
-              <input type="text" readOnly onPaste={handlePaste} name="confirmation" value={formData.confirmation} onChange={handleChange} />
+              <input
+                type="text"
+                readOnly
+                onPaste={handlePaste}
+                name="confirmation"
+                value={formData.confirmation}
+                onChange={handleChange}
+              />
             ) : (
-              <input type="text" name="confirmation" value={formData.confirmation} disabled />
+              <input
+                type="text"
+                name="confirmation"
+                value={formData.confirmation}
+                disabled
+              />
             )}
-            <div className="block-container"><label><input type="checkbox" className="checkbox" checked={confirmed} onChange={handleMainConfirmationToggle} /> Mark as "Confirmed"</label></div>
+            <div className="block-container">
+              <label>
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  checked={confirmed}
+                  onChange={handleMainConfirmationToggle}
+                />{" "}
+                Mark as "Confirmed"
+              </label>
+            </div>
           </label>
 
           {/* Family Members Section */}
           <h1>Family Members</h1>
           {formData.familyMembers.map((member, index) => (
             <div key={index} className="family-member">
-              <label>Family Member Name: <input type="text" name="name" value={member.name} onChange={(e) => handleFamilyMemberChange(index, e)} /></label>
-              <label>Date of Birth: <input type="text" readOnly onPaste={handlePaste} name="baptism" value={member.baptism} onChange={(e) => handleFamilyMemberChange(index, e)} /> </label>
+              <label>
+                Family Member Name:
+                <input
+                  type="text"
+                  name="name"
+                  value={member.name}
+                  onChange={(e) => handleFamilyMemberChange(index, e)}
+                />
+              </label>
+              <label>
+                Date of Birth:
+                <input
+                  type="text"
+                  readOnly
+                  onPaste={handlePaste}
+                  name="dob"
+                  value={member.dob}
+                  onChange={(e) => handleFamilyMemberChange(index, e)}
+                />
+              </label>
 
               {/* Family Member Baptism */}
               <label>
                 Baptism Date:
                 {!member.baptised ? (
-                  <input type="text" readOnly name="baptism" onPaste={handlePaste} value={member.baptism} onChange={(e) => handleFamilyMemberChange(index, e)} />
+                  <input
+                    type="text"
+                    readOnly
+                    name="baptism"
+                    onPaste={handlePaste}
+                    value={member.baptism}
+                    onChange={(e) => handleFamilyMemberChange(index, e)}
+                  />
                 ) : (
-                  <input type="text" name="baptism" value={member.baptism} disabled />
+                  <input
+                    type="text"
+                    name="baptism"
+                    value={member.baptism}
+                    disabled
+                  />
                 )}
-                <div className="block-container"><label><input type="checkbox" onPaste={handlePaste} className="checkbox" checked={member.baptised} onChange={() => handleBaptisedToggle(index)} /> Mark as "Baptised"</label></div>
+                <div className="block-container">
+                  <label>
+                    <input
+                      type="checkbox"
+                      className="checkbox"
+                      checked={member.baptised}
+                      onChange={() => handleBaptisedToggle(index)}
+                    />
+                    Mark as "Baptised"
+                  </label>
+                </div>
               </label>
 
               {/* Family Member Confirmation */}
               <label>
                 Confirmation Date:
                 {!member.confirmed ? (
-                  <input type="text" readOnly name="confirmation" value={member.confirmation} onChange={(e) => handleFamilyMemberChange(index, e)} />
+                  <input
+                    type="text"
+                    readOnly
+                    name="confirmation"
+                    value={member.confirmation}
+                    onChange={(e) => handleFamilyMemberChange(index, e)}
+                  />
                 ) : (
-                  <input type="text" name="confirmation" value={member.confirmation} disabled />
+                  <input
+                    type="text"
+                    name="confirmation"
+                    value={member.confirmation}
+                    disabled
+                  />
                 )}
-                <div className="block-container"><label><input type="checkbox" className="checkbox" checked={member.confirmed} onChange={() => handleConfirmationToggle(index)} /> Mark as "Confirmed"</label></div>
+                <div className="block-container">
+                  <label>
+                    <input
+                      type="checkbox"
+                      className="checkbox"
+                      checked={member.confirmed}
+                      onChange={() => handleConfirmationToggle(index)}
+                    />
+                    Mark as "Confirmed"
+                  </label>
+                </div>
               </label>
 
-              <button type="button-member" className="removeMember" onClick={() => removeFamilyMember(index)}>Remove Family Member</button>
+              <button
+                type="button-member"
+                className="removeMember"
+                onClick={() => removeFamilyMember(index)}
+              >
+                Remove Family Member
+              </button>
             </div>
           ))}
-          <button type="button-member"  onClick={addFamilyMember}>Add Family Member</button>
+          <button type="button-member" onClick={addFamilyMember}>
+            Add Family Member
+          </button>
 
-          <button type="submit" onClick={success}>Add User</button>
+          <button type="submit" onClick={success}>
+            Add User
+          </button>
         </form>
       </main>
     </div>
@@ -209,4 +387,3 @@ const AdminDashboard = () => {
 };
 
 export default AdminDashboard;
-
